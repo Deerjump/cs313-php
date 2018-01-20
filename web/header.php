1e59fd3429fccf3608581789d0a7224844d74865
Jed Billman <bil12003@byui.edu>
2018-01-20 15:35:23 -0700

    setting up initial homepage

--- web/header.php
+++ web/header.php
@@ -1,5 +1,7 @@
-<h1>Welcome to my Website!</h1>
-<ul>
-	<li><a href="home.php">Home</a></li>
-	<li><a href="assignments.php">Assignments</a></li>
-</ul>
+<h1>Welcome to my Website!</h1>\
+<nav>
+	<ul>
+		<li><a class="<?php echo $current == "home" ? "current" : "" ?>" href="home.php">Home</a></li>
+		<li><a class="<?php echo $current == "assignments" ? "current" : "" ?>" href="assignments.php">Assignments</a></li>
+	</ul>
+</nav>

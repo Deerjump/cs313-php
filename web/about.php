163e47bf0a432a943f008aaf152d1c95f8797d12
Jed Billman <bil12003@byui.edu>
2018-01-20 22:52:57 -0700

    Added all files needed for base website

---
+++ web/about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> About Me! </title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<?php include "header.php" ?>
+	</header>
+	<div class="bodywrapper">
+		<h1>About Me!</h1>
+	</div>
+</body>
+</html>
--- web/header.php
+++ web/header.php
@@ -1,7 +1,13 @@
-<h1>Welcome to my Website!</h1>
+<h1>CS 313</h1>
+
+<?php 
+	$current = basename($_SERVER['PHP_SELF']); 
+?>
+
 <nav>
 	<ul>
-		<li><a class="<?php echo $current == "home" ? "current" : "" ?>" href="home.php">Home</a></li>
-		<li><a class="<?php echo $current == "assignments" ? "current" : "" ?>" href="assignments.php">Assignments</a></li>
+		<li><a class="<?php echo $current == "home.php" ? "current" : "" ?>" href="home.php">Home</a></li>
+		<li><a class="<?php echo $current == "assignments.php" ? "current" : "" ?>" href="assignments.php">Assignments</a></li>
+		<li><a class="<?php echo $current == "about.php" ? "current" : "" ?>" href="about.php">About Me</a></li>
 	</ul>
 </nav>

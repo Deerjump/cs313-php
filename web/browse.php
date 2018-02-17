1ba8254d0f4d156f2aaabdc59c810e7d28482fef
Jed Billman <billman377@gmail.com>
2018-02-17 15:16:56 -0700

    Adding more flavor text

--- web/browse.php
+++ web/browse.php
@@ -36,15 +36,15 @@ $_SESSION['cart']['prod6'] += $_GET['prod6'];
 				<div class="col-md-4">
 					<div class="card">
 					  <div class="card-header">
-					  	<h4 class="card-title">Option 1</h4>
+					  	<h4 class="card-title">Budget PC</h4>
 					  </div>
 					  <div class="card-body">
-					    <p class="card-text">A very budget PC designed to play relatively older games or do some <strong>light</strong> video editing</p>
+					    <p class="card-text">Very budget PC designed to play relatively older games or do some <strong>light</strong> video editing</p>
 					    <ul class="list-group list-group-flush">
-    						<li class="list-group-item">Intel i3-6100</li>
-						    <li class="list-group-item">GTX 750ti</li>
-						    <li class="list-group-item">8gb DDR4 2166 Mhz</li>
+    						<li class="list-group-item">Ryzen 5 2400G 4 Cores</li>
+						    <li class="list-group-item">8GB DDR4 2166 Mhz</li>
 						    <li class="list-group-item">1 TB 7200 RPM HDD</li>
+						    <li class="list-group-item"></li>
 					    </ul>
 					    <br>
 					    <p class="card-text"> $499.99</p>
@@ -56,15 +56,15 @@ $_SESSION['cart']['prod6'] += $_GET['prod6'];
 				<div class="col-md-4">
 					<div class="card">
 					  <div class="card-header">
-					  	<h4 class="card-title">Option 2</h4>
+					  	<h4 class="card-title">Budget Gaming PC</h4>
 					  </div>
 					  <div class="card-body">
-					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <p class="card-text">Budget PC designed for modern games at low/medium settings</p>
 					    <ul class="list-group list-group-flush">
-    						<li class="list-group-item">Processor</li>
-						    <li class="list-group-item">GPU</li>
-						    <li class="list-group-item">Ram</li>
-						    <li class="list-group-item">Hard Drive</li>
+    						<li class="list-group-item">Intel i5-7400 4 Cores</li>
+						    <li class="list-group-item">Nvidia GTX 1050ti</li>
+						    <li class="list-group-item">8GB DDR4 2166 Mhz</li>
+						    <li class="list-group-item">1 TB 7200 RPM HDD</li>
 					    </ul>
 		    			<br>
 					    <p class="card-text"> $699.99</p>
@@ -76,15 +76,15 @@ $_SESSION['cart']['prod6'] += $_GET['prod6'];
 				<div class="col-md-4">
 					<div class="card">
 					  <div class="card-header">
-					  	<h4 class="card-title">Option 3</h4>
+					  	<h4 class="card-title">Mid-Tier Gaming PC</h4>
 					  </div>
 					  <div class="card-body">
-					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <p class="card-text">Very capable build for 1080p gaming at high settings</p>
 					    <ul class="list-group list-group-flush">
-    						<li class="list-group-item">Processor</li>
-						    <li class="list-group-item">GPU</li>
-						    <li class="list-group-item">Ram</li>
-						    <li class="list-group-item">Hard Drive</li>
+    						<li class="list-group-item">Ryzen 5 1600x 6 Core </li>
+						    <li class="list-group-item">Nvidia GTX 1060 6GB</li>
+						    <li class="list-group-item">8GB DDR4 2166 Mhz Ram</li>
+						    <li class="list-group-item">1 TB 7200 RPM HDD</li>
 					    </ul>
 					    <br>
 					    <p class="card-text"> $999.99</p>
@@ -104,10 +104,10 @@ $_SESSION['cart']['prod6'] += $_GET['prod6'];
 					  <div class="card-body">
 					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 					    <ul class="list-group list-group-flush">
-    						<li class="list-group-item">Processor</li>
-						    <li class="list-group-item">GPU</li>
-						    <li class="list-group-item">Ram</li>
-						    <li class="list-group-item">Hard Drive</li>
+    						<li class="list-group-item">Intel i7-7700 4 Core</li>
+						    <li class="list-group-item">Nvidia GTX 1070 </li>
+						    <li class="list-group-item">16GB DDR4 2400 Mhz Ram</li>
+						    <li class="list-group-item">256GB SSD, 1 TB 7200 RPM HDD</li>
 					    </ul>
 					    <br>
 					    <p class="card-text"> $1499.99 </p>
@@ -124,10 +124,10 @@ $_SESSION['cart']['prod6'] += $_GET['prod6'];
 					  <div class="card-body">
 					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 					    <ul class="list-group list-group-flush">
-    						<li class="list-group-item">Processor</li>
-						    <li class="list-group-item">GPU</li>
-						    <li class="list-group-item">Ram</li>
-						    <li class="list-group-item">Hard Drive</li>
+    						<li class="list-group-item">Intel i7-8700k 6 Cores</li>
+						    <li class="list-group-item">Nvidia GTX 1080</li>
+						    <li class="list-group-item">16GB DDR4 3000 Mhz Ram</li>
+						    <li class="list-group-item">500GB SSD, 1 TB 7200 RPM HDD</li>
 					    </ul>
 					    <br>
 					    <p class="card-text"> $1999.99</p>
@@ -144,10 +144,10 @@ $_SESSION['cart']['prod6'] += $_GET['prod6'];
 					  <div class="card-body">
 					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 					    <ul class="list-group list-group-flush">
-    						<li class="list-group-item">Processor</li>
-						    <li class="list-group-item">GPU</li>
-						    <li class="list-group-item">Ram</li>
-						    <li class="list-group-item">Hard Drive</li>
+    						<li class="list-group-item">Ryzen Threadripper 1900X</li>
+						    <li class="list-group-item">Nvidia GTX 1080ti</li>
+						    <li class="list-group-item">32GB DDR4 3000 Mhz</li>
+						    <li class="list-group-item">1 TB SSD, 2 TB 7200 RPM HDD</li>
 					    </ul>
 					    <br>
 					    <p class="card-text"> $2499.99.00</p>

2dda26306f7eb52012060c2aedfa7a0de9f85a99
Jed Billman <billman377@gmail.com>
2018-03-07 17:07:50 -0700

    backend changes

--- web/header.php
+++ web/header.php
@@ -9,28 +9,28 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-xlg-1">
-					<li><a class="btn <?php echo $current == "index.php" ? "btn-primary" : "btn-outline-primary" ?>" href="index.php">Home</a></li>
+					<li><a class="btn btn-header <?php echo $current == "index.php" ? "btn-primary" : "btn-outline-primary" ?>" href="index.php">Home</a></li>
 				</div>
 
 				<div class="col-xlg-1">
-					<li><a class="btn <?php echo $current == "assignments.php" ? "btn-primary" : "btn-outline-primary" ?>" href="assignments.php">Assignments</a></li>
+					<li><a class="btn btn-header <?php echo $current == "assignments.php" ? "btn-primary" : "btn-outline-primary" ?>" href="assignments.php">Assignments</a></li>
 				</div>
 		
 				<div class="col-xlg-1">
-					<li><a class="btn <?php echo $current == "browse.php" ? "btn-primary" : "btn-outline-primary" ?>" href="browse.php">Shopping</a></li>
+					<li><a class="btn btn-header <?php echo $current == "browse.php" ? "btn-primary" : "btn-outline-primary" ?>" href="browse.php">Shopping</a></li>
 				</div>
 
 				<div class="col-xlg-2">
 					
 				</div>
 				<div class="col-xlg-1">
-					<li><a class="btn <?php echo $current == "cart.php" ? "btn-primary" : "btn-outline-primary" ?>" href="cart.php">Cart</a></li>
+					<li><a class="btn btn-header <?php echo $current == "cart.php" ? "btn-primary" : "btn-outline-primary" ?>" href="cart.php">Cart</a></li>
 				</div>
 				<div class="col-xlg-1">
-					<li><a class="btn <?php echo $current == "forum.php" ? "btn-primary" : "btn-outline-primary" ?>" href="forum.php">Forum</a></li>
+					<li><a class="btn btn-header <?php echo $current == "forum.php" ? "btn-primary" : "btn-outline-primary" ?>" href="forum.php">Forum</a></li>
 				</div>
 				<div class="col-xlg-1">
-					<li><a class="btn  <?php echo $current == "about.php" ? "btn-primary" : "btn-outline-primary" ?>" href="about.php">About Me</a></li>
+					<li><a class="btn btn-header <?php echo $current == "about.php" ? "btn-primary" : "btn-outline-primary" ?>" href="about.php">About Me</a></li>
 				</div>
 			</div>
 		</div>

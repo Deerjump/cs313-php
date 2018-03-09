81cba05ac5ecb6ebfa7a2b24998bb7739ec83955
Jed Billman <bil12003@byui.edu>
2018-03-09 10:30:51 -0700

    added more styling

--- web/forum.php
+++ web/forum.php
@@ -22,7 +22,7 @@
 				<div class="card-header">
 					<div class="card-title">
 						<div class="row">
-							<div class="col-md-10">
+							<div class="col-md-10">	
 								Forum Threads <br>
 								<form method="GET">
 									<input type="text" name="query" placeholder="Keyword" value="<?php echo $query ?>">
@@ -30,7 +30,6 @@
 								</form>
 							</div>
 							<div class="col-md-2">
-
 								<a class="btn btn-success" href="new-thread.php">Create New Thread</a>
 							</div>
 						</div>
@@ -40,15 +39,14 @@
 					<div class="card-text">
 						<?php
 							
-								foreach ($db->query("SELECT * FROM thread INNER JOIN account ON thread.author_id = account.account_id WHERE LOWER(title) LIKE LOWER('%$query%') OR LOWER(username) LIKE LOWER('%$query%')")  as $row) {
+								foreach ($db->query("SELECT * FROM thread INNER JOIN account ON thread.author_id = account.account_id WHERE LOWER(title) LIKE LOWER('%$query%') OR LOWER(username) LIKE LOWER('%$query%') ORDER BY thread_id")  as $row) {
 									$title = $row['title'];
 									$author = $row['username'];
 									$id = $row['thread_id'];
 									
-									echo "<div class='row'>";
-									echo "<div class= 'col-md-4'><strong><a href='view-thread.php?id=$id'>$title</a></strong></div>";
-									echo "<div class= 'col-md-4'>" . " " .  "</div>";
-									echo "<div class= 'col-md-4'>" .  "written by: <strong>$author</strong>" . "</div></div>";
+									echo "<div class='row index-row'>";
+									echo "<div class= 'col-md-9'><h2><strong><a href='view-thread.php?id=$id'>$title</a></strong></h2></div>";
+									echo "<div class= 'col-md-3'>" .  "<h4>written by: <strong>$author</strong></h4>" . "</div></div>";
 
 								}
 								if(count($row)==0){
